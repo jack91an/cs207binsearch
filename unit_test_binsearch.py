0f24f31a546e71315e5f61f44cbe03b8858bc144
fqian <fqian91@gmail.com>
2016-09-30 06:41:57 -0400

    travis integration

--- unit_test_binsearch.py
+++ unit_test_binsearch.py
@@ -5,46 +5,47 @@ from binsearch import binary_search
 
 def test_original():
     input = list(range(5))
-    self.assertEqual(binary_serach(input,5), 5)
+    assert binary_serach(input, 2) == 2
 
 def test_nan():
     input = [np.nan]
-    with self.assertRaises(TypeError):
-        binary_search(input,2)
+    with raises(TypeError):
+        binary_search(input, np.nan)
 
 def test_numeric():
     input=['a', 'b', 7, 10]
-    with self.assertRaises(TypeError):
-        binary_search(input,3)
+    with raises(TypeError):
+        binary_search(input,'a')
 
 def test_empty():
-    self.assertEqual(binary_search([],1),-1)
+    with raises(ValueError):
+        binary_search([],1)
 
 def test_one_element():
-    self.assertEqual(ut.binary_search([1],1),-1)
+    assert binary_search([1],1) == 0
 
 def test_two_element():
-    self.assertEqual(binary_search([1,2],1),-1)
+    assert binary_search([1,2],1) == 0
 
 def test_boundary():
     input = list(range(5))
-    self.assertEqual(binary_search(input,1,3,1),-1)
+    assert binary_search(input,1,3,1) == -1
 
 def test_needle_out_of_range():
     input = list(range(5))
-    self.assertEqual(binary_search(input,11),-1)
-    self.assertEqual(binary_search(input,-1),-1)
+    assert binary_search(input,6) == -1
+    assert binary_search(input,-1) == -1
 
 def test_extremes():
     input = list(range(5))
-    self.assertEqual(binary_search(input,9),9)
-    self.assertEqual(binary_search(input,0),0)
+    assert binary_search(input,4) == 4
+    assert binary_search(input,0) == 0
 
 def test_multiple():
     input = [3]*10
-    self.assertEqual(binary_search(input,3),0)
+    assert binary_search(input,3) == 0
 
 def test_overflow():
     input = list(range(5))
-    self.assertEqual(binary_search(input,np.inf+1),-1)
+    assert binary_search(input,np.inf+1) == -1
             
